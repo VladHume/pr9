5d2cdbf91f53584a245c84b3dedb5b7b51feab69
VladHume <152102439+VladHume@users.noreply.github.com>
2025-06-17 18:39:45 +0300

    Update task9_2.c

--- task9_2.c
+++ task9_2.c
@@ -1,11 +1,8 @@
-import subprocess
+#include <stdio.h>
+#include <stdlib.h>
 
-def cat_shadow():
-    try:
-        output = subprocess.check_output(['sudo', 'cat', '/etc/shadow']).decode()
-        print(output)
-    except subprocess.CalledProcessError:
-        print("Failed to read /etc/shadow. Check sudo permissions.")
-
-if __name__ == "__main__":
-    cat_shadow()
+int main() {
+    printf("Вміст /etc/shadow:\n");
+    system("sudo cat /etc/shadow");
+    return 0;
+}
